54f2666d06d64d970fb5eb31a77abf3668f648c1
Albro3459 <brodsky.alex22@gmail.com>
2024-05-18 15:33:08 -0500

    fixed shortcut a little

--- ClipboardHistory/ClipboardHistory/ClipboardHistoryApp.swift
+++ ClipboardHistory/ClipboardHistory/ClipboardHistoryApp.swift
@@ -15,16 +15,22 @@ struct ClipboardHistoryApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     let persistenceController = PersistenceController.shared
+    var clipboardMonitor: ClipboardMonitor?
 
+    init() {
+        self.clipboardMonitor = ClipboardMonitor()
+        self.clipboardMonitor?.startMonitoring()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
-//        .commands {
-//            SidebarCommands()
-//        }
-//        .windowToolbarStyle(UnifiedWindowToolbarStyle(showsTitle: false))
+        //        .commands {
+        //            SidebarCommands()
+        //        }
+        //        .windowToolbarStyle(UnifiedWindowToolbarStyle(showsTitle: false))
     }
 }
 
@@ -43,30 +49,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
-
+    
     func toggleWindowVisibility() {
-//        print("Cmd-Shift-C pressed: Toggling window visibility")
+        //        print("Cmd-Shift-C pressed: Toggling window visibility")
         
         let now = Date()
         if let lastToggleTime = lastToggleTime, now.timeIntervalSince(lastToggleTime) < 0.33 {
-//            print("Toggle too fast, ignoring.")
+            //            print("Toggle too fast, ignoring.")
             return
         }
         self.lastToggleTime = now
         
         DispatchQueue.main.async {
             if let window = self.window {
-                if window.isVisible {
-//                    print("hiding")
-                    window.orderOut(nil)
-                } else {
-//                    print("showing")
+                if !window.isVisible {
                     window.makeKeyAndOrderFront(nil)
+                    window.collectionBehavior = .canJoinAllSpaces
                     NSApplication.shared.activate(ignoringOtherApps: true)
+                }
+                else {
+                    window.orderOut(nil)
                 }
             }
         }
     }
+    
     
     func applicationDidFinishLaunching(_ notification: Notification) {
         setupGlobalHotKey()
@@ -75,13 +82,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             let screen = window.screen ?? NSScreen.main!
             let windowWidth: CGFloat = 300
             let windowHeight: CGFloat = 500
-
+            
             let xPosition = screen.visibleFrame.maxX - windowWidth
             let yPosition = screen.visibleFrame.minY
-
+            
             let frame = CGRect(x: xPosition, y: yPosition, width: windowWidth, height: windowHeight)
             window.setFrame(frame, display: true)
-//            window.level = .floating
+            window.level = .floating
+            window.collectionBehavior = .canJoinAllSpaces
             self.window = window
         }
     }
